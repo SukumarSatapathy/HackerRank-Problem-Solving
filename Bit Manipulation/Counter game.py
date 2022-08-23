ee542e03b4b1799a31f74885678dad4e526ee548
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-08-23 19:20:34 +0530

    Counter game Question
    Louise and Richard have developed a numbers game. They pick a number and check to see if it is a power of 2. If it is, they
    divide it by 2. If not, they reduce it by the next lower number which is a power of 2. a Whoever reduces the number to 1 wins the
    game. Louise always starts.
    Given an initial value, determine who wins the game.
    
    Example
    n = 132
    It's Louise's turn first. She determines that 132 is not a power of 2. The next lower power of 2 is 128, So she subtracts that from
    132 and passes 4 to Richard. 4 is a power of 2, SO Richard divides it by 2 and passes 2 to Louise. Likewise, 2 is a power so she
    divides it by 2 and reaches 1. She wins the game.
    Update If they initially set counter to 1, Richard wins. Louise cannot make a move so she loses.
    
    Function Description
    Complete the counterGame function in the editor below.
    counterGame has the following parameter(s):
    int n: the initial game counter value
    
    Returns
    string: either Richard or Louise
    
    Input Format
    The first line contains an integer t, the number of testcases.
    Each of the next t lines contains an integer n, the initial value for each game.
    
    Constraints
    • 1 < t≤ 10
    • 1 <= n<= 2^64 - 1

---
+++ Bit Manipulation/Counter game.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n):
+    if n == 1:
+        return 'Richard'
+    no_of_plays = 0
+    
+    while n != 1:
+        if math.floor(math.log2(n)) == math.ceil(math.log2(n)):
+            n //= 2
+        else:
+            lowest_power = math.floor(math.log2(n))
+            n -= pow(2,lowest_power)
+        no_of_plays +=1
+    
+    if no_of_plays % 2 == 0:
+        return 'Richard'
+    else:
+        return 'Louise'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
