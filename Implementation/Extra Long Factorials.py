ceb498c57e0aaaffd0295d92a7e8517f330d8bff
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-08-23 17:39:15 +0530

    Extra Long Factorials Question
    The factorial of the integer n, written n!, is defined as:
    n! n × (n 1) × (n 2) x . . . × 3x2x1
    Calculate and print the factorial of a given integer. a
    For example, f n = 30, we calculate 30 × 29 × 28 X . . . × 2 × 1 and get 265252859812191058636308480000000.
    
    Function Description
    Complete the extraLongFactorials function in the editor below. It should print the result and return.
    extraLongFactorials has the following parameter(s):
    n: an integer
    Note: Factorials of n > 20 can't be stored even in a 64 bit long long variable. Big integers must be used for such
    calculations. Languages like Java, Python, Ruby etc. can handle big integers, but we need to write additional code in C/C++ to
    handle huge values.
    We recommend solving this challenge using BigIntegers.
    
    Input Format
    Input consists of single integer n
    
    Constraints
    1 < n ≤ 100
    
    Output Format
    Print the factorial of n.

---
+++ Implementation/Extra Long Factorials.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    f = 1
+    for i in range(1,n+1):
+        f *= i
+    print(f)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
