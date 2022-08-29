a3365a90df6d268a77dc2e5862d3c2713dc363e2
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-08-29 20:16:23 +0530

    Question Description
    Marc loves cupcakes, but he also likes to stay fit. Each cupcake has a calorie count, and Marc can walk a distance to expend
    those calories. If Marc has eaten j cupcakes so far, after eating a cupcake with c calories he must walk at least 21 x c miles to
    maintain his weight.
    
    Example
    calorie = 5, 10, 7
    If he eats the cupcakes in the order shown, the miles he will need to walk are
    (20 x 5) + (21 x 10) + (22 x 7) = 5 + 20 + 28 = 53. This is not the minimum, though, so we need to test other orders of
    consumption. In this case, our minimum miles is calculated as (2° × 10) + (21 × 7) + (2" × 5) = 10 + 14 + 20 = 44.
    Given the individual calorie counts for each of the cupcakes, determine the minimum number of miles Marc must walk to
    maintain his weight. Note that he can eat the cupcakes in any order.
    
    Function Description
    Complete the marcsCakewalk function in the editor below.
    marcsCakewalk has the following parameter(s):
    int calorie[n]: the calorie counts for each cupcake
    
    Returns
    long: the minimum miles necessary
    
    Input Format
    The first line contains an integer n, the number of cupcakes in calorie.
    The second line contains n space-separated integers, calorie i].
    
    Constraints
    • 1≤ n ≤ 40
    • 1 ≤ cli ≤ 1000

---
+++ Greedy/Marc's Cakewalk.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    calorie.sort(reverse = True)
+    minimum = 0
+    for i in range(len(calorie)):
+        minimum += calorie[i]*(2**i)
+    return minimum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
