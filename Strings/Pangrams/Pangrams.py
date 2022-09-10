89c84914bbbb72bc010e7110ab217257ee460ef5
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-10 19:32:17 +0530

    Create Pangrams.py

---
+++ Strings/Pangrams/Pangrams.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    string = s.upper()
+    for i in range(65,91):
+        if chr(i) not in string:
+            return "not pangram"
+    return "pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
