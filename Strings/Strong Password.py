e953b0c9e8096d81e56298b106688963b6546e4c
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-10 19:15:03 +0530

    Create Strong Password.py

---
+++ Strings/Strong Password.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumNumber' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. STRING password
+#
+
+
+def minimumNumber(n, password):
+    count = 0    
+    if any(i.isdigit() for i in password)==False:
+        count+=1
+    if any(i.islower() for i in password)==False:
+        count+=1
+    if any(i.isupper() for i in password)==False:
+        count+=1
+    if any(i in '!@#$%^&*()-+' for i in password)==False:
+        count+=1
+    return max(count,6-n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
+
