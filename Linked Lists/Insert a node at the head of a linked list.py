eaf2efa2180f123e8eb3f900716b99d353ab90bb
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-11 21:44:01 +0530

    Update Insert a node at the head of a linked list.py

--- Linked Lists/Insert a node at the head of a linked list.py
+++ Linked Lists/Insert a node at the head of a linked list.py
@@ -44,3 +44,18 @@ def insertNodeAtHead(llist, data):
     return head
 
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+    
+    fptr.close()
