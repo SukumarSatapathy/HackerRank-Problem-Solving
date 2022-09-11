da0c386b756501a8d1c58c2819db98f797f0dc60
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-11 21:45:06 +0530

    Update Print the Elements of a Linked List.py

--- Linked Lists/Print the Elements of a Linked List.py
+++ Linked Lists/Print the Elements of a Linked List.py
@@ -43,3 +43,12 @@ def printLinkedList(head):
         head = head.next
 
 if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
