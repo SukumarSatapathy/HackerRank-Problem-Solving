b758a24e15291578e0c49ba39ccf028cd1b73dd7
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-11 21:41:53 +0530

    Create Print the Elements of a Linked List.py

---
+++ Linked Lists/Print the Elements of a Linked List.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    while head is not None:
+        print(head.data)
+        head = head.next
+
+if __name__ == '__main__':
